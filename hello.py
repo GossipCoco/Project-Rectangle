60fa14ac17f3fa7f35fa53b3b537c842bf197356
GossipCoco <gossipcoco@hotmail.fr>
2024-07-11 15:54:33 +0200

    first commit by local

---
+++ hello.py
@@ -0,0 +1,8 @@
+print ('hello world')
+
+a = float(input("Entrez le premier nombre : "))
+b = float(input("Entrez le second nombre : "))
+print(f"Somme: {a + b}")
+print(f"Différence: {a - b}")
+print(f"Produit: {a * b}")
+print(f"Quotient: {a / b}")
